eb6c763e4e98dfbf5733fc596179b4138ac70ad8
Max Walker <maxwell.a.walker@gmail.com>
2016-06-01 10:44:00 -0400

    Add Email action

--- src/actions/Email.js
+++ src/actions/Email.js
@@ -1,3 +1,44 @@
-export default class Email {
+import { EmailTemplate } from 'email-templates'
+import { templateProps } from '../helpers';
 
+
+export default class Email {
+  constructor(transport) {
+    this.mailer = nodemailer.createTransport(transport);
+  }
+
+  sendEmail(props) {
+
+    return (data) => {
+      const { from, to, subject, html: _html, template } = templateProps(props);
+
+      let html = _html;
+
+      if (template) {
+        templated = new EmailTemplate(templateDir)
+
+        templated.render(data, function (err, result) {
+          if (err) {
+            console.error(err)
+            throw err;
+          }
+
+          transport.sendMail({
+            from,
+            to,
+            subject,
+            html: result.html,
+          }, function(err, info) { });
+        })
+      } else {
+        transport.sendMail({
+          from,
+          to,
+          subject,
+          html,
+        }, function(err, info) { });
+      }
+    }
+
+  }
 }
